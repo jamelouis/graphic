3fe4d4624287f0e253845f723a4a13604f9bb969
jamelouis <jamelouis2015@gmail.com>
2018-03-27 19:56:11 +0800

    release configuration

--- D3DRender.cpp
+++ D3DRender.cpp
@@ -7,6 +7,7 @@
 #include <fstream>
 
 #include <xnamath.h>
+#include <algorithm>
 
 #include "SimpleCamera.h"
 
--- DxObjModel.cpp
+++ DxObjModel.cpp
@@ -2,6 +2,7 @@
 #include "tinyobj/include/tiny_obj_loader.h"
 #include "DxUtils.h"
 #include <unordered_map>
+#include <algorithm>
 
 #define ObjNormal
 
